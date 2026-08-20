7ffb911b8682647cbceecc833cdf2ba92c48ca53
dileepr01 <62456568+dileepr01@users.noreply.github.com>
2026-08-20 05:39:40 +0530

    Remove global daily discovery script

--- scripts/daily-jobs.ts
+++
@@ -1,8 +0,0 @@
-import { runDailyJobs } from '@/lib/pipeline'
-
-runDailyJobs()
-  .then((metrics) => console.log('JobPilot daily run completed', metrics))
-  .catch((error) => {
-    console.error('JobPilot daily run failed', error)
-    process.exitCode = 1
-  })
